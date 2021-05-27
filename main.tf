3e9a8c3d3eea717e44fb66213182d7a9ef9e3572
alexandreponte2 <79060274+alexandreponte2@users.noreply.github.com>
2021-05-27 15:07:48 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "who" {
+  ami               = "ami-09e67e426f25ce0d7"
+  availability_zone = "us-east-1a"
+  instance_type     = "t2.micro"
+  key_name          = "who-key"
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "15"
+  }
+  tags = {
+    Name = "who"
+  }
+}
+output "dns" {
+  value = aws_instance.who.public_dns
+}
