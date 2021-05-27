76c40b3b45627788c5566806a27114364c90ba9b
alexandreponte2 <79060274+alexandreponte2@users.noreply.github.com>
2021-05-27 16:48:52 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "who" {
   ami               = "ami-09e67e426f25ce0d7"
   availability_zone = "us-east-1a"
   instance_type     = "t2.micro"
-  key_name          = "who-key"
+  key_name          = var.key_name
   root_block_device {
     volume_type = "gp2"
     volume_size = "15"
